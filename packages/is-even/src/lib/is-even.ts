6607e43b8144f2ab3ce36bb5a371b08d519ce182
Arjan van Wieringen <avanwieringen@Arjans-Air.home>
2023-04-01 11:21:40 +0200

    Added prettier

--- packages/is-even/src/lib/is-even.ts
+++ packages/is-even/src/lib/is-even.ts
@@ -1,3 +1,9 @@
+import { say } from 'cowsay';
+
 export function isEven(x: number): boolean {
   return x % 2 === 0;
+}
+
+export function random(): string {
+  return say({ text: 'Hello World' });
 }
