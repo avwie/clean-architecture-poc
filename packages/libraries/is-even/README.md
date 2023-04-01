44ab8c9d61eedc017b87b82a7245f3c96b8c56f2
Arjan van Wieringen <avanwieringen@Arjans-Air.home>
2023-04-01 12:20:25 +0200

    Not working grouping

---
+++ packages/libraries/is-even/README.md
@@ -0,0 +1,11 @@
+# libraries-is-even
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build libraries-is-even` to build the library.
+
+## Running unit tests
+
+Run `nx test libraries-is-even` to execute the unit tests via [Jest](https://jestjs.io).
--- packages/libraries/is-even/jest.config.ts
+++ packages/libraries/is-even/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: 'is-even',
-  preset: '../../jest.preset.js',
+  displayName: 'libraries-is-even',
+  preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/is-even',
+  coverageDirectory: '../../../coverage/packages/libraries/is-even',
 };
