9f118f374d9a2d853259bfb837edb11849fc6156
Arjan van Wieringen <avanwieringen@Arjans-Air.home>
2023-04-02 18:43:28 +0200

    Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: '2019',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {
+  }
+}
--- packages/libraries/is-odd/src/is-odd.ts
+++ packages/libraries/is-odd/src/is-odd.ts
@@ -1,5 +1,5 @@
-import { isEven } from "@avwie/is-even";
+import { isEven } from '@avwie/is-even'
 
-export function isOdd(value: number): boolean {
-  return !isEven(value);
+export function isOdd (value: number): boolean {
+  return !isEven(value)
 }
