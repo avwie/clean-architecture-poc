86984dfcfef21f0a923482e6398a44096daa9a51
Arjan van Wieringen <avanwieringen@Arjans-MacBook-Air.local>
2023-04-03 10:42:38 +0200

    Web server

---
+++ packages/apps/web-server/src/index.ts
@@ -0,0 +1,24 @@
+import express, { type Express, type Request, type Response } from 'express'
+import { isEven } from '@avwie/is-even'
+import { isOdd } from '@avwie/is-odd/dist/is-odd'
+
+const app: Express = express()
+const port = 8080
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.get('/is-even/:number', (req: Request, res: Response) => {
+  const number = parseInt(req.params.number, 10)
+  res.send(`${number} is ${isEven(number) ? 'even' : 'odd'}`)
+})
+
+app.get('/is-odd/:number', (req: Request, res: Response) => {
+  const number = parseInt(req.params.number, 10)
+  res.send(`${number} is ${isOdd(number) ? 'odd' : 'even'}`)
+})
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
--- packages/libraries/is-even/src/is-even.ts
+++ packages/libraries/is-even/src/is-even.ts
@@ -1,6 +1,3 @@
-import { say } from 'cowsay'
-
 export function isEven (n: number): boolean {
-  console.log(say({ text: 'Hello' }))
   return n % 2 === 0
 }
